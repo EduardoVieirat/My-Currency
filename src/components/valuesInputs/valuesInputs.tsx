ccab40df1d8dd357360f4affd0d776813957fc01
EduardoOdev <duduvieiratou@outlook.com>
2024-07-01 19:16:43 -0300

    [feat]: including and using the zustand states on inputs

--- src/components/valuesInputs/valuesInputs.tsx
+++ src/components/valuesInputs/valuesInputs.tsx
@@ -1,82 +1,72 @@
 "use client";
 
-import { FormatValues } from "../../services/formatValues";
+import { useInputsStore } from "../../hooks/hooks";
+import { formatCurrencyInput } from "../../services/formatValues";
 
-type TValuesInput = {
-  taxInput: string;
-  setTaxInput: (ti: string) => void;
-  currencyInput: string;
-  setCurrencyInput: (ci: string) => void;
-  errorTax: boolean;
-  errorCurrency: boolean;
-  setErrorCurrency: (ec: boolean) => void;
-  setErrorTax: (et: boolean) => void;
-};
+export default function ValuesInput() {
+  const {
+    setCurrencyInput,
+    currencyInput,
+    taxInput,
+    setTaxInput,
+    errorCurrencyInput,
+    errorTaxInput,
+    setErrorCurrencyInput,
+    setErrorTaxInput,
+  } = useInputsStore();
 
-export default function ValuesInput({
-  taxInput,
-  setTaxInput,
-  currencyInput,
-  setCurrencyInput,
-  errorTax,
-  errorCurrency,
-  setErrorCurrency,
-  setErrorTax,
-}: TValuesInput) {
   function handleCurrency(e: { target: { value: string } }) {
-    const formatedCurrency = FormatValues(e.target.value);
-
-    setErrorCurrency(false);
-
-    setCurrencyInput(`$ ${formatedCurrency}`);
+    const valueCurrency = e.target.value;
+    setErrorCurrencyInput(false);
+    setCurrencyInput(valueCurrency);
   }
 
-  function handleTax(e: { currentTarget: { value: string } }) {
-    const formatedTax = FormatValues(e.currentTarget.value);
-
-    setErrorTax(false);
-
-    setTaxInput(`% ${formatedTax}`);
+  function handleTax(e: { target: { value: string } }) {
+    const valueTax = e.target.value;
+    setErrorTaxInput(false);
+    setTaxInput(valueTax);
   }
 
   return (
-    <section className="items-start justify-between w-96 h-32 text-2xl">
-      <div className="flex flex-col">
+    <section className="items-start justify-between min-w-96 h-32 text-2xl">
+      <div className="flex flex-col w-full">
         <p className="font-medium">Dólar</p>
         <input
           type="text"
           placeholder="$ 1,00"
-          value={currencyInput}
+          value={formatCurrencyInput(currencyInput).replace("$", "$ ")}
           onChange={handleCurrency}
-          className={`drop-shadow-md shadow-slate-400 w-36 h-14 ${
-            errorCurrency ? `border-red-600` : `border-gray-300`
+          className={`drop-shadow-md shadow-slate-400 max-w-36 h-14  ${
+            errorCurrencyInput ? `border-red-600` : `border-gray-300`
           }  border rounded px-4`}
         />
-        {errorCurrency && (
+        {errorCurrencyInput && (
           <span className="text-base text-red-600 my-2 w-36">
             Preencha o campo com um valor
           </span>
         )}
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full">
         <p className="font-medium">Taxa do Estado</p>
         <input
           type="text"
-          placeholder="0,08 %"
-          value={taxInput}
+          placeholder="% 0,08"
+          value={formatCurrencyInput(taxInput).replace("$", "% ")}
           onChange={handleTax}
           maxLength={8}
           className={`drop-shadow-md shadow-slate-400 w-36 h-14 ${
-            errorTax ? `border-red-600 ` : `border-gray-300`
+            errorTaxInput ? `border-red-600 ` : `border-gray-300`
           }  border rounded px-4`}
         />
-        {errorTax && (
+        {errorTaxInput && (
           <span className="text-base text-red-600 w-36">
             Preencha o campo com um valor
           </span>
         )}
+
       </div>
+
     </section>
   );
 }
